681a2e0609f75cfcf8036518d12d8356005caa4b
breiti78 <remo@breitis.ch>
2021-12-24 21:19:01 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,67 @@
+radio.setGroup(1)
+pins.setPull(DigitalPin.P13, PinPullMode.PullNone)
+pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
+pins.setPull(DigitalPin.P15, PinPullMode.PullNone)
+pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P15) == 0) {
+        radio.sendString("P15")
+    } else if (pins.digitalReadPin(DigitalPin.P13) == 0) {
+        radio.sendString("P13")
+    } else if (pins.digitalReadPin(DigitalPin.P16) == 0) {
+        radio.sendString("P16")
+    } else if (pins.digitalReadPin(DigitalPin.P14) == 0) {
+        radio.sendString("P14")
+    } else if (input.buttonIsPressed(Button.A)) {
+        radio.sendString("A")
+    } else if (input.buttonIsPressed(Button.B)) {
+        radio.sendString("B")
+    } else {
+        if (pins.analogReadPin(AnalogPin.P2) > 550 && (pins.analogReadPin(AnalogPin.P1) > 400 && pins.analogReadPin(AnalogPin.P1) < 600)) {
+            radio.sendString("F")
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `)
+        } else if (pins.analogReadPin(AnalogPin.P2) < 450 && (pins.analogReadPin(AnalogPin.P1) > 400 && pins.analogReadPin(AnalogPin.P1) < 600)) {
+            radio.sendString("R")
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
+                `)
+        } else if (pins.analogReadPin(AnalogPin.P1) < 450 && (pins.analogReadPin(AnalogPin.P2) > 400 && pins.analogReadPin(AnalogPin.P2) < 600)) {
+            radio.sendString("L")
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+        } else if (pins.analogReadPin(AnalogPin.P1) > 550 && (pins.analogReadPin(AnalogPin.P2) > 400 && pins.analogReadPin(AnalogPin.P2) < 600)) {
+            radio.sendString("R")
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
+        } else {
+            radio.sendString("S")
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+        }
+    }
+})
